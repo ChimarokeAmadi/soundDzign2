3342c8ad71c020cc1a327c3408cc247442139cdc
Chimaroke Amadi <chirokeamadi@gmail.com>
2024-06-08 15:49:28 +0100

    added background color to the body

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -9,7 +9,7 @@ const Header = () => {
 			<div className='cta'>
 				<p className='inter fs-16 red-line'>Sound Design Masterclass</p>
 				<h2 className='alegreya fs-40'>Learn the Art of Sound Design</h2>
-				<button className='fs-16'>Demo Lesson</button>
+				<button className='inter fs-16'>Demo Lesson</button>
 			</div>
 		</section>
 	);
