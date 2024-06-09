1afc9e7b61bc5c7f010e6896b28191dcad410e32
Chimaroke Amadi <chirokeamadi@gmail.com>
2024-06-09 21:30:41 +0100

    added tablet version to topic and blog sections

--- src/components/Blog/index.jsx
+++ src/components/Blog/index.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import "./Blog.css";
 import Daw from "../../assets/Rectangle 13_Topic-images.png";
 import Mixing from "../../assets/Thumbnail-guitarguitar.png";
-import Vox from "../../assets/Thumbnail-harmonies_harmonies.png";
+import Vox from "../../assets/Thumbnail_vox.png";
 
 const Blog = () => {
 	return (
 		<section id='blog'>
 			<h2 className='inter fs-40 yellow-line'>Latest Posts</h2>
 			<div className='posts-container'>
-				<div className='blog-post'>
+				<div className='blog-post' id='daw-post'>
 					<img src={Daw} alt='' />
 					<p className='alegreya fs-18'>
 						How To Use Drum Machine in Logic Pro X
@@ -23,7 +23,7 @@ const Blog = () => {
 					<div className='tag fs-13 alegreya'>Mixing</div>
 				</div>
 
-				<div className='blog-post'>
+				<div className='blog-post' id='span2'>
 					<img src={Vox} alt='' />
 					<p className='alegreya fs-18'>
 						The Real Power of Harmonies in Music Production
