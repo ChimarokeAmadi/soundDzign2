5883b00bad691680fae9c24232198ca51b56037f
Chimaroke Amadi <chirokeamadi@gmail.com>
2024-06-09 22:06:23 +0100

    added tablet version to testimonial section

--- src/components/Testimonials/index.jsx
+++ src/components/Testimonials/index.jsx
@@ -11,27 +11,33 @@ const Testimonials = () => {
 			<div className='testimonials-container'>
 				<div className='student-info'>
 					<img src={peter} alt='' className='student-image' />
-					<p className='student-name alegreya fs-20'>Peter Adams</p>
-					<p className='student-testimonial fs-16 alegreya'>
-						This is a great course. I got to learn a lot.
-					</p>
+					<div className='student-details'>
+						<p className='student-name alegreya fs-20'>Peter Adams</p>
+						<p className='student-testimonial fs-16 alegreya'>
+							This is a great course. I got to learn a lot.
+						</p>
+					</div>
 				</div>
 
 				<div className='student-info'>
 					<img src={robert} alt='' className='student-image' />
-					<p className='student-name alegreya fs-20'>Robert Fox</p>
-					<p className='student-testimonial fs-16 alegreya'>
-						I got to learn a lot about Music Production with this course. Thanks
-						:)
-					</p>
+					<div className='student-details'>
+						<p className='student-name alegreya fs-20'>Robert Fox</p>
+						<p className='student-testimonial fs-16 alegreya'>
+							I got to learn a lot about Music Production with this course.
+							Thanks :)
+						</p>
+					</div>
 				</div>
 
 				<div className='student-info'>
 					<img src={emily} alt='' className='student-image' />
-					<p className='student-name alegreya fs-20'>Emily Smith</p>
-					<p className='student-testimonial fs-16 alegreya'>
-						Awesome! Great job!!
-					</p>
+					<div className='student-details'>
+						<p className='student-name alegreya fs-20'>Emily Smith</p>
+						<p className='student-testimonial fs-16 alegreya'>
+							Awesome! Great job!!
+						</p>
+					</div>
 				</div>
 			</div>
 		</section>
